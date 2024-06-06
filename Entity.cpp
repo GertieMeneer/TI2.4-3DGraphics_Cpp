9fb7a2b79615423cff858f5cf0c09075ee54609a
GertieMeneer <gertjan.vat@icloud.com>
2024-06-06 20:10:27 +0200

    Removed unused renderablecomponent

--- Entity.cpp
+++ Entity.cpp
@@ -1,9 +1,14 @@
 #include "Entity.h"
 
+
+
 Entity::Entity() :
 
     transform(TransformComponent()),
     velocity(nullptr),
-    renderable(nullptr),
     collider(nullptr),
     lifetime(nullptr) {}
+
+void Entity::update() {
+
+}
